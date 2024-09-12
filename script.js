e55a019f61c4a0febef13a9fc75170a0b84c78e0
Muh Rizki Pratama <rizkipratama0550@gmail.com>
2024-09-12 09:06:29 +0700

    Add File

---
+++ script.js
@@ -0,0 +1,41 @@
+// Normalisasi
+const buttonNormalisasi = document.getElementById('button-normalisasi');
+const modalNormalisasi = document.getElementById('modal-normalisasi');
+const closeNormalisasi = document.getElementById('close-normalisasi');
+
+// Denormalisasi
+const buttonDenormalisasi = document.getElementById('button-denormalisasi');
+const modalDenormalisasi = document.getElementById('modal-denormalisasi');
+const closeDenormalisasi = document.getElementById('close-denormalisasi');
+
+// ERD
+const buttonErd = document.getElementById('button-erd');
+const modalErd = document.getElementById('modal-erd');
+const closeErd = document.getElementById('close-erd');
+
+// Normalisasi
+buttonNormalisasi.addEventListener('click', function(){
+    modalNormalisasi.style.display = 'flex';
+});
+
+closeNormalisasi.addEventListener('click', function(){
+    modalNormalisasi.style.display = 'none';
+});
+
+// Denormalisasi
+buttonDenormalisasi.addEventListener('click', function(){
+    modalDenormalisasi.style.display = 'flex';
+});
+
+closeDenormalisasi.addEventListener('click', function(){
+    modalDenormalisasi.style.display = 'none';
+});
+
+// ERD
+buttonErd.addEventListener('click', function(){
+    modalErd.style.display = 'flex';
+});
+
+closeErd.addEventListener('click', function(){
+    modalErd.style.display = 'none';
+});
